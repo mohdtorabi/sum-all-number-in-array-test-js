f70a247c700ee7aad6374feb16416a82e765897f
mohdtorabi <mitq8.mt@gmail.com>
2020-10-31 20:12:16 +0000

    recursion :xxx

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+/* eslint-disable func-style */
+const sumItems = function(array) {
+  let sumNum = 0;
+  array.forEach((num) => {
+    if (Array.isArray(num)) {
+      sumNum += sumItems(num);
+    } else {
+      sumNum += num;
+      //console.log(sumNum);
+    }
+  });
+  return sumNum;
+  
+};
 
+let arr = [1, [2, 3], 4, 5];
+
+console.log(sumItems(arr));
 module.exports = sumItems;
+
+
+
